8d6a67c3ce0892e4f0e4f863e2c479eefe34187c
Ciper <1634076598@qq.com>
2020-03-30 09:07:08 +0800

    First LakerhomeApplication

---
+++ src/main/java/com/ciper/lakerhome/LakerhomeApplication.java
@@ -0,0 +1,14 @@
+package com.ciper.lakerhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakerhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakerhomeApplication.class, args);
+        System.out.println("hello world!");
+    }
+
+}
---
+++ src/test/java/com/ciper/lakerhome/LakerhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciper.lakerhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakerhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
